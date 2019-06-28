d4887b7090ed2f14f79b3a89d7865d68bf7ef9b5
whitehat92 <ze_correia93@hotmail.com>
2019-06-28 11:36:31 +0100

    Update pcap.py

--- pcap.py
+++ pcap.py
@@ -1,13 +1,12 @@
 """
 DISCLAIMER: as always, please don't judge my code. You were once a learner as well
 """
-
-
+# -*- coding: utf-8 -*-
 from scapy.all import *
 import sys
+import time
 from scapy.utils import RawPcapReader
 import argparse
-
 
 lerpcap = str(sys.argv[1])
 timeprompt = str(input("Do you want to print with time? (Y/N): "))
@@ -20,9 +19,10 @@ if timeprompt == "Y" or timeprompt == "y" or timeprompt == "":
     for a in s.items():
         print(a)
     print("-------- TIME ----------")
-    for b, c in s.items():
-        for x in c:
-            print(x.time, b)
+    for a, variables in s.items():
+        for tm in variables:
+            opentime = time.strftime('%A, %d/%m/%y, %I:%M:%S %p', time.localtime(tm.time))
+            print(opentime, a)
     hexprompt = str(input("Also wanna print hexdump? "))
     if hexprompt == "y":
         print(pcap.hexdump())
